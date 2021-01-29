3737c8b02e944b2afa8237239eadd9155a5e88ee
AreebahKhan-nr <AreebahKhan-nr@github.com>
2021-01-29 17:24:44 +0530

    Making page using flexbox

--- src/UI/Components/Footer/Footer.js
+++ src/UI/Components/Footer/Footer.js
@@ -1,17 +1,18 @@
-import React, {Component } from 'react';
-class Footer extends Component{
-    render(){
-        return(
-            
+import React, { Component } from 'react';
+import classes from './Footer.module.css';
+class Footer extends Component {
+    render() {
+        return (
+
             <footer class="footer">
-                <div class="footer-list">
-                    <a href="#" class="footer-link">Home</a>
-                    <a href="#" class="footer-link" >Trending</a>
-                    <a href="#" class="footer-link">New</a>
-                    <a href="#" class="footer-link">Contact us</a>
-                </div>
-                
-            </footer>
+                <div class={classes.footerList}>
+                    <a href="#" class="footerLink">Home</a>
+                    <a href="#" class="footerLink" >Trending</a>
+                    <a href="#" class="footerLink">New</a>
+                    <a href="#" class="footerLink">Contact us</a>
+                </div>                
+          </footer>
         )
+    }
 }
-}   
+export default Footer;
--- src/pages/Movie/Movie.js
+++ src/pages/Movie/Movie.js
@@ -5,44 +5,52 @@ import Footer from '../../UI/Components/Footer/Footer.js'
 import classes from './Movie.module.css';
 
 class Movie extends Component {
-    render() {
-        
+    render() {   
         return (
-            <div className={classes.header}>
-                <a href="#default" class="logo">MOVIE DESCRIPTION PAGE</a>
-                <div className="header-right">
-                    <a class="active" href="#home">Home</a>
-                    <a href="#contact">Contact</a>
-                    <a href="#about">About</a>
-                    <div class="search-container">
+
+            <React.Fragment>
+                <div class={classes.topnav}>
+                    <a class="active" href="#home">Movie Page</a>
+                    <a href="#contact">Home</a>
+                    <a href="#about">Trending</a>
+                    <a href="#contact">New In</a>
+                    <a href="#contact">Home</a>
+                    <div class="searchContainer">
                         <form action="/action_page.php">
-                                <input type="text" placeholder="Search.." name="search" />
-                                <button type="submit"><i class="fa fa-search">GO</i></button>
+                        <input type="text" placeholder="Search Here." name="search"/>
+                        <button type="submit"><i class="fa fa-search">Search</i></button>
                         </form>
                     </div>
                 </div>
-            </div>
 
-            // <React.Fragment>
-            //     <div className={[classes.container, classes.grey].join(' ')}>
-
-            //         <nav class="nav-list">
-            //             <a href="#" class="nav-link center">Home</a>
-            //             <a href="#" class="nav-link center" >Trending</a>
-            //             <a href="#" class="nav-link center">New</a>
-            //             <a href="#" class="nav-link center">
-            //                 <form action="/action_page.php">
-            //                     <input type="text" placeholder="Search.." name="search" />
-            //                     <button type="submit"><i class="fa fa-search"></i></button>
-			// 	            </form>
-			//             </a>
-            //         </nav>
-            //     </div>
-            //     div
-            //     <Footer/>
-            // </React.Fragment >
+                {/* <div className={classes.header}>
+                    <a href="#default" class="logo">Movie Site</a>
+                    <div class="headerRight">
+                        <a class="active" href="#home">Home</a>
+                        <a href="#contact">Contact</a>
+                        <a href="#about">About</a>
+                        <div class="search-container">
+                            <form action="poster.jpg">
+                                <input type="text" placeholder="Search.." name="search"/>
+                                <button type="submit"><i class="searching"></i></button>
+                            </form>
+                        </div>
+                    </div>
+                </div> */}
+                <div class={classes.container-2}>
+                    <div class="c-1">
+                        &emsp;&emsp;&emsp;&emsp;<img src="poster.jpg" width="300" height="400" center /> &emsp;	
+                    </div>
+                    <div class="c-2">
+                        <h3>About the movie<br/></h3>
+                        <p>More about it</p><br/><br/>
+                        <h3>Payment and viewing options</h3>
+                        <p>Lorem Ipsum ...........</p>
+                    </div>
+                </div>
+                <Footer />
+            </React.Fragment >
         )
     }
 }
-
 export default Movie;
