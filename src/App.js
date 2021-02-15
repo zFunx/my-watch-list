15bd0e500d318633420270f70f4da04f5aa7396a
AreebahKhan-nr <AreebahKhan-nr@github.com>
2021-02-15 21:17:03 +0530

    Login Page start

--- src/App.js
+++ src/App.js
@@ -7,41 +7,38 @@ import Home from './pages/Home/Home';
 import Search from './pages/Search/Search';
 import Navbar from './UI/Components/Navbar/Navbar';
 import { Component } from 'react';
+import Login from './admin/Login/Login';
 class App extends Component {
-  state = {
-    q: '',// search query
-    searchResults: []
-  }
+	state = {
+		q: '',// search query
+		searchResults: []
+	}
 
-  search = (event) => {
-    const inp = event.currentTarget;
-    const q = inp.value;
-	    axios.get('/movie?name_contains={searchResults}')
-		.then(res => {
-			const searchResults=res.data;
-			this.setState({searchResults});
-		})
-	// 	)}
-    // 	.then( response => {
-    //   results: this.setState.searchResults;
-	 this.setState({q: q, searchResults: ['inception', 'minions']})
-    // console.log('user entered search query');
-  };
+	search = (event) => {
+		const inp = event.currentTarget;
+		const q = inp.value;
+		axios.get(`/movies?name_contains=${q}`)
+			.then(res => {
+				const searchResults = res.data;
+				this.setState({ q, searchResults });
+			});
+	};
 
-  render() {
-    let mainPage = <Route exact path="/" component={Home} />;
-    if (this.state.searchResults.length > 0) {
-      mainPage = <Route exact path="/" render={() => <Search results={this.state.searchResults} />} />;
-    }
+	render() {
+		let mainPage = <Route exact path="/" component={Home} />;
+		if (this.state.searchResults.length > 0) {
+			mainPage = <Route exact path="/" render={() => <Search results={this.state.searchResults} />} />;
+		}
 
-    return (
-      <div className="App">
-        <Navbar onSearch={this.search} />
-        {mainPage}
-        <Route exact path="/movie/:code" component={Movie} />
-      </div>
-    );
-  }
+		return (
+			<div className="App">
+				<Navbar onSearch={this.search} />
+				{mainPage}
+				<Route exact path="/movie/:code" component={Movie} />
+				<Route exact path="/admin" component={Login} />
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/admin/Login/Login.js
@@ -0,0 +1,20 @@
+
+import React, { Component } from 'react';
+
+import classes from './Login.module.css';
+class Login extends Component { 
+        render() {
+            return(
+                <React.Fragment>
+                    Login Page
+                    {/* 
+                    1. Check if already logged in
+                    2. if not,Show username and password input with login button.
+                    3. Store the login token
+                    4. Redirect to a dashboard page after logged in
+                    */}
+                </React.Fragment>          
+            )
+        }
+}
+export default Login;
